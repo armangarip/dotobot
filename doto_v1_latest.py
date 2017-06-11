adf72a963e00d61fce05aa1e52b4b66a5bca3b7c
arman <arman.garip@ozu.edu.tr>
2017-06-11 17:40:40 +0300

    changed architecture

--- doto_v1_latest.py
+++ doto_v1_latest.py
@@ -3,15 +3,27 @@ import aiohttp
 import asyncio
 import logging
 import time
+from discord.ext import commands
+
+description = 'A discord bot'
+
+bot = commands.Bot(command_prefix='!', description=description)
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
 
 timed_users = []
 
-client = discord.Client()
+# client = discord.Client()
 dota_channel = discord.Object(id='89274408438353920')
 bot_channel = discord.Object(id='321781885699358741')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 def if_user_available_remove(user):
     for auser in timed_users:
@@ -27,14 +39,14 @@ def if_user_available_remove(user):
 #         await asyncio.sleep(60)
 
 async def check_timers():
-    await client.wait_until_ready()
-    while not client.is_closed:
+    await bot.wait_until_ready()
+    while not bot.is_closed:
         now = time.time()
         for user in timed_users:
             if now > user.timeout:
                 print("User timed out: " + user.user.name)
                 timed_users.remove(user)
-                await client.send_message(bot_channel, user.user.name + ' is no longer playing (' + str(len(timed_users)) + '/5) -timeout')
+                await bot.say(user.user.name + ' is no longer playing (' + str(len(timed_users)) + '/5) -timeout')
         await asyncio.sleep(20)
 
 
@@ -47,7 +59,7 @@ async def check_timers():
 
 class TimedUser:
 
-    def __init__(self, user, timeout_mins):
+    def __init__(self, user, timeout_mins = 60):
         self.user = user
         now = time.time()
         self.timeout = now + (timeout_mins * 60)
@@ -55,86 +67,100 @@ class TimedUser:
     def get_remaining_time(self):
         return int((self.timeout - time.time()) / 60)
 
-async def who(channel):
+
+@bot.command(pass_context=True)
+async def who(ctx):
     if timed_users:
         print_str = 'There are ' + str(len(timed_users)) + ' people waiting to play dota\n' 
         for user in timed_users:
             print_str += user.user.name + ' - ' + str(user.get_remaining_time()) + ' minutes\n'
-        # print_str += "are up to play Dota. (" + str(len(timed_users)) + '/5)'
-        await client.send_message(channel, print_str)
+        await bot.say(print_str)
     else:
-        await client.send_message(channel, "No one wants to play Dota right now.")
+        await bot.say("No one wants to play Dota right now.")
 
+
+@bot.command()
 async def alert():
     print_str = 'go ' 
     for user in timed_users:
         print_str += user.user.mention + ' '
-    await client.send_message(dota_channel, print_str)
+    await bot.say(print_str)
 
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+@bot.command(pass_context=True)
+async def dota(ctx, time : int = 60):
+    user = ctx.message.author
 
-@client.event
-async def on_message(message):
+    if if_user_available_remove(user):
+        await bot.send_message(bot_channel, user.name + ' is no longer playing (' + str(len(timed_users)) + '/5)')
+    else:
+        try:
+            new_player = TimedUser(user, time)
+        except Exception:
+            bot.say("Wrong type for timeout minutes")
+            return
+        timed_users.append(new_player)
+        await bot.say(user.name + ' is up to play Dota! (' + str(len(timed_users)) + '/5)')
 
-    user = message.author
-
-    if message.content.startswith('!dota'):
-        if if_user_available_remove(user):
-            await client.send_message(bot_channel, user.name + ' is no longer playing (' + str(len(timed_users)) + '/5)')
-        else:
-            splitted_msg = message.content.split(' ')
-            if len(splitted_msg) == 1:
-                new = TimedUser(user, 60)
-                timed_users.append(new)
-                await client.send_message(bot_channel, user.name + ' is up to play Dota! (' + str(len(timed_users)) + '/5)')
-            elif len(splitted_msg) == 2:
-                #Check if given timeout duration is an integer
-                if splitted_msg[1].isdigit():
-                    new = TimedUser(user, int(splitted_msg[1]))
-                    timed_users.append(new)
-                    await client.send_message(bot_channel, user.name + ' is up to play Dota for ' + str(splitted_msg[1])+ ' minutes! (' + str(len(timed_users)) + '/5)')
-                else:
-                    await client.send_message(bot_channel, 'Wrong type for timeout minutes.')
-
-            if len(timed_users) == 5:
-                await alert()
-
-    elif message.content.startswith('!who'):
-        await who(message.channel)
-
-    elif message.content.startswith('!alert'):
-        await alert()
-
-    elif message.content.startswith('!ilker'):
-        print(user.name + " ilker'd")
-        await client.delete_message(message)
-
-    elif message.content.startswith('!delete'):
-        await client.delete_message(message)
-        print("Deleted message: " + message.content)
-
-    elif message.content.startswith('!msg'):
-        await msg_format()
-        
-    elif message.content.startswith('!help'):
-        await client.send_message(bot_channel, "Commands for dotobot:\n\n" + 
-                                                   "!dota - toggle dota availability (now with timeout!)\n" + 
-                                                   "!who - print people up to play dota\n" + 
-                                                   "!alert - alert people who want to play dota\n" + 
-                                                   "!ilker - delete this message\n" + 
-                                                   "!help - print this help")
+        if len(timed_users) == 5:
+            await alert()
 
 
+# @bot.event
+# async def on_message(message):
+#     user = message.author
+#
+#     if message.content.startswith('!dota'):
+#         if if_user_available_remove(user):
+#             # await bot.say(bot_channel, user.name + ' is no longer playing (' + str(len(timed_users)) + '/5)')
+#         else:
+#             splitted_msg = message.content.split(' ')
+#             if len(splitted_msg) == 1:
+#                 new = TimedUser(user, 60)
+#                 timed_users.append(new)
+#                 # await bot.say(bot_channel, '{0} is up to play dota ({1}/5)'.format(user.name, str(len(timed_users))))
+#             elif len(splitted_msg) == 2:
+#                 #Check if given timeout duration is an integer
+#                 if splitted_msg[1].isdigit():
+#                     new = TimedUser(user, int(splitted_msg[1]))
+#                     timed_users.append(new)
+#                     await bot.say(bot_channel, user.name + ' is up to play Dota for ' + str(splitted_msg[1])+ ' minutes! (' + str(len(timed_users)) + '/5)')
+#                 else:
+#                     await bot.say(bot_channel, 'Wrong type for timeout minutes.')
+#
+#             if len(timed_users) == 5:
+#                 await alert()
+#
+#     # elif message.content.startswith('!who'):
+#     #     await who(message.channel)
+#
+#     # elif message.content.startswith('!alert'):
+#     #     await alert()
+#     #
+#     # elif message.content.startswith('!ilker'):
+#     #     print(user.name + " ilker'd")
+#     #     await bot.delete_message(message)
+#     #
+#     # elif message.content.startswith('!delete'):
+#     #     await bot.delete_message(message)
+#     #     print("Deleted message: " + message.content)
+#
+#     # elif message.content.startswith('!msg'):
+#     #     await msg_format()
+#
+#     elif message.content.startswith('!help'):
+#         await bot.say(bot_channel, "Commands for dotobot:\n\n" +
+#                                                    "!dota - toggle dota availability (now with timeout!)\n" +
+#                                                    "!who - print people up to play dota\n" +
+#                                                    "!alert - alert people who want to play dota\n" +
+#                                                    "!ilker - delete this message\n" +
+#                                                    "!help - print this help")
 
-client.loop.create_task(check_timers())
+
+
+bot.loop.create_task(check_timers())
 # client.loop.create_task(print_who())
-client.run('MzA5NzAyNzY3NjEyNzg4NzQ2.DBidow.VGf7Wrc60J0iUzTLGyNTFWfpQQc')
+bot.run('MzA5NzAyNzY3NjEyNzg4NzQ2.DBidow.VGf7Wrc60J0iUzTLGyNTFWfpQQc')
 
 
 
